5042e189db1d7808719d06361bd1bfe316356bca
ilya-step <77782372+ilya-step@users.noreply.github.com>
2022-05-24 17:54:53 +0500

    Add files via upload

--- assets/js/customScript.js
+++ assets/js/customScript.js
@@ -81,6 +81,68 @@ $('.popup__close').click(function () {
 })
 
 
+// анимация при загрузке страницы
+$(document).ready(function () {
+	$('.img-animate').addClass('_action');
+	$('body').addClass('_action');
+	// Анимация при скролле
+	function onEntry(entry) {
+		entry.forEach(change => {
+			if (change.isIntersecting) {
+				change.target.classList.add('_action');
+			}
+		});
+	}
+	let options = {
+		threshold: [0.1]
+	};
+	let observer = new IntersectionObserver(onEntry, options);
+	let elements = document.querySelectorAll('.animated');
+	for (let elm of elements) {
+		observer.observe(elm);
+	}
+});
+
+
+
+// dropdown menu
+let intervalId;
+document.querySelectorAll('.dropdown-toggle').forEach(e => {
+	e.addEventListener('click', e => {
+		const menu = e.currentTarget.dataset.path;
+		document.querySelectorAll('.dropdown-menu').forEach(e => {
+			if (!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+				e.classList.remove('menu-active');
+				e.classList.remove('open');
+				document.querySelector(`[data-target=${menu}]`).classList.add('menu-active');
+				document.querySelector(`[data-path=${menu}]`).classList.add('menu-active');
+				intervalId = setTimeout(() => {
+					document.querySelector(`[data-target=${menu}]`).classList.add('open');
+				}, 0);
+			}
+
+			if (document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+				clearTimeout(intervalId);
+				document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active');
+				document.querySelector(`[data-path=${menu}]`).classList.remove('menu-active');
+				intervalId = setTimeout(() => {
+					document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+				}, 0);
+			}
+
+			window.onclick = e => {
+				if (e.target == document.querySelector(`[data-target=${menu}]`) || e.target == document.querySelector(`[data-path=${menu}]`)) {
+					return;
+				} else {
+					document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active');
+					document.querySelector(`[data-target=${menu}]`).classList.remove('open');
+					document.querySelector(`[data-path=${menu}]`).classList.remove('menu-active');
+				}
+			}
+		});
+	});
+});
+
 
 // маска на телефон
 let selector = document.querySelectorAll('input[type="tel"]');
